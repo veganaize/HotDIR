f186b2b74db27e89eb16b7b4fa7b6f0d1a448344
veganaiZe <veganaiZe@protonmail.com>
2024-03-10 12:21:05 -0700

    Refactor main function location

--- hd.c
+++ hd.c
@@ -55,6 +55,42 @@ TCHAR volume_name[MAX_PATH + 1] = { 0 };
 char  *root_path = "x:\\";
 
 
+int main(int argc, char* argv[])
+{
+    //SHORT  console_width;
+    char   search_path[MAX_PATH];
+    char   search_string[MAX_PATH];
+
+    //struct mystruct_t {
+    //    int i,
+    //    long l,
+    //} strA;
+
+    original_attributes = screen_info_t.wAttributes; /* Save console info */
+    console_width = screen_info_t.srWindow.Right;    /* Get console width */
+    console_height = screen_info_t.srWindow.Bottom
+                     - screen_info_t.srWindow.Top;
+
+
+    build_initial_search_string(search_path, search_string);
+    get_console_info();
+    process_cmdline_args(argc,
+                         argv,
+                         search_drive,
+                         search_path,
+                         search_string);
+    display_header(search_path, console_width);
+    fixup_path(search_path);
+    process_files(search_handle, search_path);
+
+    FindClose(search_handle);
+
+    display_footer();
+    restore_console();
+    return 0;
+}
+
+
 int restore_console()
 {
     SetConsoleTextAttribute(hConsole, original_attributes);
@@ -476,41 +512,5 @@ int process_cmdline_args(int argc, char *argv[],
 
     }  /* End while */
 
-    return 0;
-}
-
-
-int main(int argc, char* argv[])
-{
-    //SHORT  console_width;
-    char   search_path[MAX_PATH];
-    char   search_string[MAX_PATH];
-
-    //struct mystruct_t {
-    //    int i,
-    //    long l,
-    //} strA;
-
-    original_attributes = screen_info_t.wAttributes; /* Save console info */
-    console_width = screen_info_t.srWindow.Right;    /* Get console width */
-    console_height = screen_info_t.srWindow.Bottom
-                     - screen_info_t.srWindow.Top;
-
-
-    build_initial_search_string(search_path, search_string);
-    get_console_info();
-    process_cmdline_args(argc,
-                         argv,
-                         search_drive,
-                         search_path,
-                         search_string);
-    display_header(search_path, console_width);
-    fixup_path(search_path);
-    process_files(search_handle, search_path);
-
-    FindClose(search_handle);
-
-    display_footer();
-    restore_console();
     return 0;
 }
